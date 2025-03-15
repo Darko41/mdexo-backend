f6acf2c0f60755bd162b8a17fce4f295401c59c8
Darko41 <kracunovicd@icloud.com>
2025-03-15 06:10:56 +0100

    Project restructure separating back and front

--- frontend/src/components/PomocPage.jsx
+++
@@ -1,9 +0,0 @@
-export default function PomocPage() {
-  return (
-    <div className="flex justify-center items-center h-screen">
-      <h1 className="text-4xl font-bold text-center">
-        Page under construction: ПОМОЋ
-      </h1>
-    </div>
-  );
-}
--- frontend/src/components/PrijavaPage.jsx
+++
@@ -1,9 +0,0 @@
-export default function PrijavaPage() {
-  return (
-    <div className="flex justify-center items-center h-screen">
-      <h1 className="text-4xl font-bold text-center">
-        Page under construction: ПРИЈАВА
-      </h1>
-    </div>
-  );
-}
