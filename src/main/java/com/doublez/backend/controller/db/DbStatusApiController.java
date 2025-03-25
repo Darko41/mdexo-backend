c4b9765392852ca266d8471c2feda8184ba8c02e
Darko41 <kracunovicd@icloud.com>
2025-03-25 20:05:26 +0100

    Add changelogs for Liquibase, delete backup.sql and log db-status to debug

--- src/main/java/com/doublez/backend/controller/db/DbStatusApiController.java
+++ src/main/java/com/doublez/backend/controller/db/DbStatusApiController.java
@@ -1,5 +1,7 @@
 package com.doublez.backend.controller.db;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,12 +17,15 @@ public class DbStatusApiController {
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
 	
+	private static final Logger logger = LoggerFactory.getLogger(DbStatusApiController.class);
+	
 	@GetMapping("/db-status")
 	public ResponseEntity<String> checkDatabase() {
 		try {
 			jdbcTemplate.queryForObject("SELECT 1", Integer.class);
 			return ResponseEntity.ok("Database is connected!");
 		} catch (Exception e) {
+			logger.error("Database connection failed!", e);
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Database connection failed!");
 		}
 	}
