003e3628dbf2ea826aefc2aef4f2876a35cffa0c
Darko41 <kracunovicd@icloud.com>
2025-06-28 01:27:42 +0200

    Add cors allowedOrigins for render

--- src/main/java/com/doublez/backend/config/CorsConfig.java
+++ src/main/java/com/doublez/backend/config/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:5173")
+                    .allowedOrigins("http://localhost:5173", "https://mdexo-frontend.onrender.com")
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                     .allowedHeaders("*")
                     .allowCredentials(true)
