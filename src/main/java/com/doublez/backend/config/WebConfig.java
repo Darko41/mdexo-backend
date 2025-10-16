4d1185a96b2289d58b7616406f510678243891ed
Darko41 <kracunovicd@icloud.com>
2025-10-16 06:03:13 +0200

    Try adding ReactRoutingController in webconfig to resolve property/id page

--- src/main/java/com/doublez/backend/config/WebConfig.java
+++ src/main/java/com/doublez/backend/config/WebConfig.java
@@ -1,6 +1,8 @@
 package com.doublez.backend.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -25,5 +27,17 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/admin/login").setViewName("admin/login");
+    }
+    
+    @Controller
+    public static class ReactRoutingController {
+        
+        @RequestMapping(value = { 
+            "/", "/property/**", "/create-listing", "/login", 
+            "/signup", "/buy", "/rent", "/sell", "/help", "/search" 
+        })
+        public String forwardToReact() {
+            return "forward:/index.html";
+        }
     }
 }
